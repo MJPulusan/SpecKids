0d427f3eea8c0f41b92c886c453f19c607f61c20
Michael Pulusan <mjpulusan@yahoo.com>
2025-04-24 15:55:53 +0000

    Initial commit

--- database/data.sql
+++ database/data.sql
@@ -1,11 +1,23 @@
 -- Use SQL insert statements to add any
 -- starting/dummy data to your database tables
 
--- EXAMPLE:
+insert into "Users" ("fullName", "userName", "hashedPassword", "createdAt")
+values
+  ('Mike', 'mikep', 'password123', now()),
+  ('Matt', 'mattp', 'qwerty123', now()),
+  ('Jayz', 'jayz', 'zxcvb123', now());
 
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+
+insert into "Schedules" ("userId", "therapyName", "timeOfDay", "daysOfWeek", "createdAt")
+values
+  (1, 'Speech Therapy', '10:00 AM', 'Friday', now()),
+  (1, 'Occupational Therapy', '2:00 PM', 'Tuesday', now()),
+  (2, 'Physical Therapy', '9:30 AM', 'Monday', now()),
+  (3, 'Behavioral Therapy', '3:00 PM', 'Thursday', now());
+
+
+insert into "TimeLimits" ("userId", "hoursLimit", "minutesLimit", "createdAt")
+values
+  (1, 1, 30, now()), /* 1 hr 30 mins */
+  (2, 2, 0, now()),  /* 2 hrs */
+  (3, 0, 45, now()); /* 45 mins */
--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,37 @@
 set client_min_messages to warning;
 
--- DANGER: this is NOT how to do it in the real world.
--- `drop schema` INSTANTLY ERASES EVERYTHING.
+-- -- DANGER: this is NOT how to do it in the real world.
+-- -- `drop schema` INSTANTLY ERASES EVERYTHING.
 drop schema "public" cascade;
 
 create schema "public";
+
+
+CREATE TABLE "public"."Users" (
+  "userId"          SERIAL PRIMARY KEY,
+  "fullName"        text NOT NULL,
+  "userName"        text NOT NULL,
+  "hashedPassword"  text NOT NULL,
+  "createdAt"       timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "Schedules" (
+  "scheduleId"      SERIAL PRIMARY KEY,
+  "userId"          int NOT NULL,
+  "therapyName"     text NOT NULL,
+  "timeOfDay"       text NOT NULL,
+  "daysOfWeek"      text NOT NULL,
+  "createdAt"       timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "TimeLimits" (
+  "limitId"         SERIAL PRIMARY KEY,
+  "userId"          int NOT NULL,
+  "hoursLimit"      int NOT NULL,
+  "minutesLimit"    int NOT NULL,
+  "createdAt"       timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "Schedules" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "TimeLimits" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
