f1a24bd51e35d533d252d543f2f75230a97db7cd
Michael Pulusan <mjpulusan@yahoo.com>
2025-04-26 23:49:30 +0000

    Added Registration functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { ParentRegForm } from './pages/ParentRegistration';
 import { ParentsMain } from './pages/ParentsMainPage';
 import { KidsRegForm } from './pages/KidsRegistration';
 import { KidsMain } from './pages/KidsMainPage.tsx';
 import SketchPage from './pages/SketchPage';
 import HomePage from './pages/HomeLogin';
+import { HashRouter as Router } from 'react-router-dom';
 
 export default function App() {
   return (
-    <Routes>
+    <Router>
       <Route path="/" element={<HomePage />} />
       <Route path="/sketchpad" element={<SketchPage />} />
       <Route path="/parent-register" element={<ParentRegForm />} />
       <Route path="/parents-main" element={<ParentsMain />} />
       <Route path="/kids-register" element={<KidsRegForm />} />
       <Route path="/kids-main" element={<KidsMain />} />
-    </Routes>
+    </Router>
   );
 }
